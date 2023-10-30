800f0035220b591dab23485e37621403bdb0aca3
Alexander Zemskov <xandr38@yandex.ru>
2023-10-30 12:12:31 +0400

    Третий урок (ред.) 1. Строковый массив заголовков полей разделён на строковые ресурсы. 2. Исключено использование supportActionBar и override методов для меню. Вместо этого: setNavigationOnClickListener setOnMenuItemClickListener. 3. Исправлены параметры ширины, либо привязок текстов и прокруток.

--- Lesson_3_Zemskov/app/src/main/java/hh/school/lesson_3_zemskov/ProfileActivityOnConstraintLayout.kt
+++ Lesson_3_Zemskov/app/src/main/java/hh/school/lesson_3_zemskov/ProfileActivityOnConstraintLayout.kt
@@ -1,11 +1,9 @@
 package hh.school.lesson_3_zemskov
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import hh.school.lesson_3_zemskov.databinding.ActivityProfileOnConstraintLayoutBinding
 import hh.school.lesson_3_zemskov.model.User
 
@@ -19,55 +17,44 @@ class ProfileActivityOnConstraintLayout : AppCompatActivity() {
         binding = ActivityProfileOnConstraintLayoutBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setSupportActionBar(binding.toolbar)
-        supportActionBar?.apply {
-            setDisplayHomeAsUpEnabled(true)
+        binding.toolbar.setNavigationOnClickListener {
+            finish()
+        }
+        binding.toolbar.setOnMenuItemClickListener {
+            Toast.makeText(applicationContext, R.string.menu_item_edit_text, Toast.LENGTH_SHORT)
+                .show()
+            true
         }
 
         with(binding) {
-            val personalDataLabels = resources.getStringArray(R.array.personal_data_labels)
-            listItemName.textViewOverline.text = personalDataLabels[0]
-            listItemSurname.textViewOverline.text = personalDataLabels[1]
-            listItemEmail.textViewOverline.text = personalDataLabels[2]
-            listItemLogin.textViewOverline.text = personalDataLabels[3]
+            listItemName.textViewOverline.text = resources.getString(R.string.label_name)
+            listItemSurname.textViewOverline.text = resources.getString(R.string.label_surname)
+            listItemEmail.textViewOverline.text = resources.getString(R.string.label_email)
+            listItemLogin.textViewOverline.text = resources.getString(R.string.label_login)
 
-            listItemRegion.textViewOverline.text = personalDataLabels[4]
+            listItemRegion.textViewOverline.text = resources.getString(R.string.label_region)
             listItemRegion.imageViewEdit.visibility = View.VISIBLE
             listItemRegion.imageViewEdit.setOnClickListener {
-                Toast.makeText(applicationContext, R.string.list_item_icon_edit_text, Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    applicationContext,
+                    R.string.list_item_icon_edit_text,
+                    Toast.LENGTH_SHORT
+                ).show()
             }
 
-            listItemExitAccount.textViewOverline.visibility = View.GONE
-            listItemExitAccount.imageViewExitAccount.visibility = View.VISIBLE
-            listItemExitAccount.textViewHeadline.text = resources.getString(R.string.exit_account_text)
-            buttonExitAccount.setOnClickListener {
-                Toast.makeText(applicationContext, R.string.exit_account_text, Toast.LENGTH_SHORT).show()
+            buttonExitAccount.root.setOnClickListener {
+                Toast.makeText(applicationContext, R.string.exit_account_text, Toast.LENGTH_SHORT)
+                    .show()
             }
 
             val user = User()
-            textViewHeader.root.text = resources.getString(R.string.header_text, user.cardNumber, user.skill)
+            textViewHeader.root.text =
+                resources.getString(R.string.header_text, user.cardNumber, user.skill)
             listItemName.textViewHeadline.text = user.name
             listItemSurname.textViewHeadline.text = user.surname
             listItemEmail.textViewHeadline.text = user.email
             listItemLogin.textViewHeadline.text = user.login
             listItemRegion.textViewHeadline.text = user.region
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.actions, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when(item.itemId) {
-            R.id.actionEdit -> {
-                Toast.makeText(applicationContext, R.string.menu_item_edit_text, Toast.LENGTH_SHORT)
-                    .show()
-                true
-            }
-
-            else -> super.onOptionsItemSelected(item)
         }
     }
 }
